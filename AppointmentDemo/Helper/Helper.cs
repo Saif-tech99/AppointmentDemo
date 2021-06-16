f85f35fee8d5127e6129f173a390d6cd0eb3919c
Saif Al-adilee <saifa9697@gmail.com>
2021-06-16 14:58:22 +0200

    Add project files.

---
+++ AppointmentDemo/Helper/Helper.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AppointmentDemo.Helper
+{
+    public static class Helper
+    {
+        public static string Admin = "Admin";
+        public static string Patiean = "Patiean";
+        public static string Doctor = "Doctor";
+
+        public static List<SelectListItem> GetRolesForDropDown()
+        {
+            return new List<SelectListItem>
+            {
+                new SelectListItem{Value= Helper.Admin, Text=Helper.Admin},
+                new SelectListItem{Value= Helper.Patiean, Text=Helper.Patiean},
+                new SelectListItem{Value= Helper.Doctor, Text=Helper.Doctor}
+            };
+        }
+    }
+}
---
+++ AppointmentDemo/Models/ViewModels/RegisterVM.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AppointmentDemo.Models.ViewModels
+{
+    public class RegisterVM
+    {
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [StringLength(100, ErrorMessage = "the min passowrd length must be 6 char")]
+        public string Password { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [Display(Name = "Confirm password")]
+        [Compare("Password", ErrorMessage = "the password does not match")]
+        public string ConfirmPassword { get; set; }
+
+        [Required]
+        [Display(Name = "Role Name")]
+        public string RoleName { get; set; }
+    }
+}
